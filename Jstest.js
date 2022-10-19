ed9a22b73aab3874bbe6dfc3e04bcea0f7bebdae
fkarisa <faith.pendo@student.moringaschool.com>
2022-10-19 09:47:26 +0300

    js

---
+++ Jstest.js
@@ -0,0 +1,17 @@
+if (1 ==1 ){console.log
+    ('Here')} else {console.log("there")
+}
+//functions
+//individual piece of work
+//syntax
+//method
+//cons myFirstFunction ={}
+const mySecondFunction =(inputone)
+//Declare output
+return Result; 
+}
+function additionFunction(oneinput, numbertwo) { }
+const favNumber = 7;
+const secondNumber =1000;
+additionFunction(favNumber,secondNumber);
+
